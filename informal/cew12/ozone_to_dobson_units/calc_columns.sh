5615774088b50f50d279615e5d018e96dd7b1c0a
Charlie Wartnaby <cew12@cam.ac.uk>
2025-07-15 16:37:16 +0100

    Fixed paths for u-dr226 output and hence updated plot which was previously using old version before change of STP pressure definition

--- informal/cew12/ozone_to_dobson_units/calc_columns.sh
+++ informal/cew12/ozone_to_dobson_units/calc_columns.sh
@@ -2,5 +2,5 @@ module purge
 module load jaspy
 ./ozone_to_dobson_units.py -i /gws/nopw/j04/ukca_vol2/2025-07-ukesm-eval/model_output/u-dr061/2005_2014_O3/u-dr061_upm.nc -o /gws/nopw/j04/ukca_vol2/2025-07-ukesm-eval/model_output/u-dr061/2005_2014_O3/u-dr061_O3_total_DU.nc
 ./ozone_to_dobson_units.py -t -i /gws/nopw/j04/ukca_vol2/2025-07-ukesm-eval/model_output/u-dr061/2005_2014_O3/u-dr061_upm.nc -o /gws/nopw/j04/ukca_vol2/2025-07-ukesm-eval/model_output/u-dr061/2005_2014_O3/u-dr061_O3_tropo_DU.nc
-./ozone_to_dobson_units.py -i /gws/nopw/j04/ukca_vol2/2025-07-ukesm-eval/model_output/u-dr226/2005_2014_O3/u-dr226_upm.nc -o /gws/nopw/j04/ukca_vol2/2025-07-ukesm-eval/model_output/u-dr061/2005_2014_O3/u-dr226_O3_total_DU.nc
-./ozone_to_dobson_units.py -t -i /gws/nopw/j04/ukca_vol2/2025-07-ukesm-eval/model_output/u-dr226/2005_2014_O3/u-dr226_upm.nc -o /gws/nopw/j04/ukca_vol2/2025-07-ukesm-eval/model_output/u-dr061/2005_2014_O3/u-dr226_O3_tropo_DU.nc
+./ozone_to_dobson_units.py -i /gws/nopw/j04/ukca_vol2/2025-07-ukesm-eval/model_output/u-dr226/2005_2014_O3/u-dr226_upm.nc -o /gws/nopw/j04/ukca_vol2/2025-07-ukesm-eval/model_output/u-dr226/2005_2014_O3/u-dr226_O3_total_DU.nc
+./ozone_to_dobson_units.py -t -i /gws/nopw/j04/ukca_vol2/2025-07-ukesm-eval/model_output/u-dr226/2005_2014_O3/u-dr226_upm.nc -o /gws/nopw/j04/ukca_vol2/2025-07-ukesm-eval/model_output/u-dr226/2005_2014_O3/u-dr226_O3_tropo_DU.nc
